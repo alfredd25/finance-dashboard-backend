f3799fedcf68b29fa5a2cbc5d4ae76fed51126fd
alfredd25 <alfredbiju7@gmail.com>
2026-04-03 14:32:41 +0530

    feat: add CORS support for cross-origin frontend compatibility

--- core/settings/base.py
+++ core/settings/base.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
@@ -28,6 +29,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware", 
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -159,3 +161,10 @@ Authorization: Bearer <access_token>
     ],
     "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],
 }
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = config(
+    "CORS_ALLOWED_ORIGINS",
+    default="http://localhost:3000,http://localhost:5173",
+).split(",")
+CORS_ALLOW_CREDENTIALS = True
